51c5528517779536c56d1a8c240419245da0a1a8
niyatisriram <niyatisriram@gmail.com>
2021-03-15 11:29:52 -0400

    comments for future reference

--- proj-skeleton/pi/partA/Permutations.java
+++ proj-skeleton/pi/partA/Permutations.java
@@ -1,4 +1,9 @@
 class Permutations{
     public static void main(String [] args){
+        int T_SUPPORT = 3;
+        double T_CONFIDENCE = 0.65;
+        //with hashmap from Pi.java, calculate support and confidence for all permutations
+        //return those whose values exceed the thresholds
+
     }
 }
--- proj-skeleton/pi/partA/Pi.java
+++ proj-skeleton/pi/partA/Pi.java
@@ -1,5 +1,9 @@
 class Pi{
 	public static void main(String[] args){
+
+		//generate call graph and map
+		//hashmap with scope1, scope2, etc. as keys and array of functions as values
+		//return hashmap
 	
 	}
 }
