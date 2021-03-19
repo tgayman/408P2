9d0a215862811cf6135efb1deefed3d82deb3a8d
niyativs <niyatisriram@gmail.com>
2021-03-19 11:46:35 -0400

    comments

--- proj-skeleton/pi/partA/Permutations.java
+++ proj-skeleton/pi/partA/Permutations.java
@@ -4,17 +4,15 @@ class Permutations{
 
     Hashmap<String, ArrayList<String>> graphMap = new HashMap();
     Hashmap<String, int> usesMap = new HashMap();
-    int T_SUPPORT = 3;
-    double T_CONFIDENCE = 0.65;
+    private final int T_SUPPORT = 3;
+    private final double T_CONFIDENCE = 0.65;
 
-    public static void main(String [] args){
-
-        //with hashmap from Pi.java, calculate support and confidence for all permutations
-        //return those whose values exceed the thresholds
-
-
-    }
-
+    /**
+     * Method to calculate the support of a pair of functions
+     * @param func1
+     * @param func2
+     * @return int support
+     */
     public int calculateSupport(String func1, String func2) {
         int count = 0;
         Iterator iterator = graphMap.entrySet().iterator();
@@ -27,10 +25,23 @@ class Permutations{
         return count;
     }
 
+    /**
+     * Method to calculate support of a single function
+     * @param func
+     * @return int support
+     */
     public int countOccurences(String func) {
         return usesMap.getValue(func);
     }
 
+    /**
+     * Method to calculate the confidence of 2 sets of functions
+     * Allows for up to 2 functions per set
+     * TODO: return error message if set size > 2
+     * @param set1
+     * @param set2
+     * @return double confidence
+     */
     public double calculateConfidence(ArrayList<String> set1, ArrayList<String> set2) {
         //set 1 support
         int set1Support = 0;
@@ -47,7 +58,15 @@ class Permutations{
         return confidence;
     }
 
+    /**
+     * Method to locate potential bugs in the program,
+     * by calculating the support and confidence for all pairs
+     * of functions and comparing them to the threshold values
+     *
+     * POTENTIAL TODO: compare with both functions' individual supports
+     */
     public void permute() {
+        //build arraylist of all functions for easier iteration
         Set<String> func = usesMap.keySet();
         ArrayList<String> functions = new ArrayList();
         Iterator<String> iterator = func.iterator();
@@ -55,14 +74,17 @@ class Permutations{
             functions.add(iterator.next());
         }
 
-        for (int i = 0; i <= functions.size(); i++) {
-            for (int j = i + 1; j <= functions.size(); j++) {
+        //find all permutations of function pairs
+        for (int i = 0; i < functions.size(); i++) {
+            for (int j = i + 1; j < functions.size(); j++) {
                 if (i == j) continue;
+                //create sets, one pair and one individual
                 ArrayList<String> set1 = {functions.get(i), functions.get(j)};
                 ArrayList<String> set2 = {functions.get(i)};
+                //calculate confidence
                 double confidence = calculateConfidence(set1, set2);
                 if (confidence > T_CONFIDENCE) {
-                    //potential bug, check support
+                    //potential bug, calculate support
                     int support = calculateSupport(set1.get(0), set1.get(1));
                     if (support > T_SUPPORT) {
                         //bug found, send data to findBugs()
@@ -73,17 +95,28 @@ class Permutations{
         }
     }
 
+    /**
+     * Method to locate and print all of the bugs found by permute()
+     * Bug is defined as when an individual function is called where
+     * it is typically called with a pair
+     * @param set1
+     * @param support
+     * @param confidence
+     */
     public void findBugs(ArrayList<String> set1, int support, double confidence) {
         //find out where one function in the set is being called without the other
         Iterator iterator = graphMap.entrySet().iterator();
         while (iterator.hasNext()) {
+            //find which scope contains the bug
             Map.Entry scope = (Map.Entry)iterator.next();
             ArrayList<String> calls = scope.getValue();
+            //checking first function in pair
             if (calls.contains(set1.get(0)) && !calls.contains(set1.get(1))) {
                 System.out.println("bug: " + set1.get(0) + " in " + scope.getKey()
                         + ", pair: " + set1.toString() + ", support: " + support
                         + ", confidence: " + confidence*100 + "%\n");
             }
+            //checking second function in pair
             else if (!calls.contains(set1.get(0)) && calls.contains(set1.get(1))) {
                 System.out.println("bug: " + set1.get(1) + " in " + scope.getKey()
                         + ", pair: " + set1.toString() + ", support: " + support
